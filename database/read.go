bd96f9336fe1898bdbf7d73689f0e99bd03face7
DongJun <you@example.com>
2025-03-17 16:47:41 +0800

    latest

--- database/read.go
+++ database/read.go
@@ -8,7 +8,8 @@ import (
 
 func QuerySingleWithField(db *gorm.DB) model.User {
 	user := model.User{}
-	result := db.First(&user, "name = ?", "dj") //根据指定的字段来检索，SELECT * FROM users WHERE name = "dj" ORDER BY id LIMIT 1;
+	//result := db.First(&user, "name = ?", "dongjun") //根据指定的字段来检索，SELECT * FROM users WHERE name = "dj" ORDER BY id LIMIT 1;
+	result := db.Where("name = ?", "dongjun").First(&user)
 	if result.Error != nil {
 		fmt.Println("查询失败:", result.Error)
 	}
@@ -17,9 +18,10 @@ func QuerySingleWithField(db *gorm.DB) model.User {
 
 func QueryManyWithField(db *gorm.DB) []model.User {
 	var users []model.User
-	db.Find(&users, "name = ?", "dj") //SELECT * FROM users WHERE name = "dj";
+	db.Where("name <> ?", "dongjun").Find(&users) //SELECT * FROM users WHERE name <> "dongjun";<>为不等于
 	if len(users) == 0 {
 		fmt.Println("查询失败:暂无记录")
 	}
 	return users
 }
+func InQuery() {}
