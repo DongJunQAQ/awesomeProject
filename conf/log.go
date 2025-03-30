e9a66db2ad0b85d8cf9f7d6732a3b6654b113538
DongJun <you@example.com>
2025-03-30 22:48:39 +0800

    latest

--- conf/log.go
+++ conf/log.go
@@ -2,15 +2,12 @@ package conf
 
 import (
 	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 )
 
 func InitDevLogger() {
-	cfg := zap.NewDevelopmentConfig()
-	cfg.EncoderConfig.EncodeTime = zapcore.TimeEncoderOfLayout("2006-01-02 15:04:05") //设置该配置的日志时间格式
-	logger, _ := cfg.Build()
-	logger.Debug("dev this is debug")
-	logger.Info("dev this is info")
-	logger.Warn("dev this is warn")
-	logger.Error("dev this is error")
+	logger, _ := zap.NewDevelopment()
+	logger.Info("dev this is info",
+		zap.String("name", "DongJun"),
+		zap.Uint8("age", 22),
+		zap.Bool("isDelete", false))
 }
