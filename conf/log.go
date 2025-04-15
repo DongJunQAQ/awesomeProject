b4a6be7f8705f39f0cfee7dca1046707b7e0046d
DongJun <you@example.com>
2025-04-15 21:02:28 +0800

    日志输出美化

--- conf/log.go
+++ conf/log.go
@@ -5,8 +5,27 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
-func encodeLevel(zapcore.Level, zapcore.PrimitiveArrayEncoder) {
+const ( //定义颜色转义码
+	Blue   = "\033[34m"
+	Yellow = "\033[33m"
+	Red    = "\033[31m"
+	Green  = "\033[32m"
+	ReSet  = "\033[0m" //每行日志输出后需要还原至默认颜色
+)
 
+func encodeLevel(level zapcore.Level, encoder zapcore.PrimitiveArrayEncoder) {
+	switch level {
+	case zapcore.DebugLevel:
+		encoder.AppendString(Green + "DEBUG" + ReSet)
+	case zapcore.InfoLevel:
+		encoder.AppendString(Blue + "INFO" + ReSet)
+	case zapcore.WarnLevel:
+		encoder.AppendString(Yellow + "WARNING" + ReSet)
+	case zapcore.ErrorLevel, zapcore.DPanicLevel, zapcore.PanicLevel, zapcore.FatalLevel:
+		encoder.AppendString(Red + "ERROR" + ReSet)
+	default:
+		encoder.AppendString(level.String())
+	}
 }
 
 func InitDevLogger() {
--- test/one.go
+++ test/one.go
@@ -2,37 +2,17 @@ package main
 
 import "fmt"
 
-type Book struct {
-	title  string
-	author string
-}
-
-func (b Book) getInfo() { //为结构体添加方法
-	//(b Book):被称作方法的值接收者，它表明getInfo函数是Book结构体类型中的一个方法，即该方法与Book类型关联。b是接收者的实例名称，可以在方法内部通过变量b来访问Book结构体中的字段
-	fmt.Printf("Book title: %s, Book author: %s\n", b.title, b.author)
-}
-
-type Library struct {
-	books []Book
-}
-
-func (l *Library) addBook(book Book) {
-	//(l *Library):被称为指针接收者，它允许方法直接操作结构体本身而非结构体的副本，一般会在结构体中写入数据时用到。这样在addBook方法内部对l.books进行的修改就会反映到原结构体上
-	l.books = append(l.books, book)
-}
-
-func (l *Library) getBooks() {
-	//对于一个结构体，要么都使用值接收者方法，要么都使用指针接收者方法；由于上面的方法使用了指针接收者所以这里也要使用
-	for _, book := range l.books {
-		book.getInfo()
-	}
+func PrintValue(v interface{}) { //接收一个空接口（任意类型）类型的参数
+	fmt.Printf("值为:%v，类型为:%T\n", v, v)
 }
 
 func main() {
-	lib := &Library{}
-	book1 := Book{title: "123", author: "dj"}
-	book2 := Book{title: "456", author: "djj"}
-	lib.addBook(book1)
-	lib.addBook(book2)
-	lib.getBooks()
+	// 定义不同类型的变量
+	num := 10
+	str := "hello"
+	arr := [3]int{1, 2, 3}
+	// 调用PrintValue函数并传入不同类型的变量
+	PrintValue(num)
+	PrintValue(str)
+	PrintValue(arr)
 }
