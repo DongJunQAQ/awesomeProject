3f967aae1c3454d5f03ff9cfa45b85545f3dab66
DongJun <you@example.com>
2025-04-28 23:05:53 +0800

    引入gin

--- main.go
+++ main.go
@@ -4,15 +4,22 @@ import (
 	"awesomeProject/conf"
 	"awesomeProject/database"
 	"awesomeProject/model"
-	"fmt"
+	"awesomeProject/web"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	//初始化数据库
 	db := database.GetGlobalDB()
 	err := db.AutoMigrate(&model.User{}) //自动迁移数据库
 	if err != nil {
 		conf.GetGlobalLogger().Panicf("数据库迁移失败")
 	}
-	fmt.Println(database.SelectFieldsQuery(db))
-	fmt.Println(database.SelectFieldsQuery(db))
+	//初始化web
+	router := gin.Default()       //创建一个默认的Gin路由器实例
+	router.GET("/ping", web.Ping) //这里web.Ping与web.Ping()的区别
+	err = router.Run()            //默认监听8080端口
+	if err != nil {
+		conf.GetGlobalLogger().Panicf("web服务启动失败:%s", err)
+	}
 }
