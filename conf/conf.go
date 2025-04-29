e2134b7547fea59c1761f3b0b2bdc75e34b8fc3a
DongJun <you@example.com>
2025-04-29 13:26:00 +0800

    规范变量名与函数名

--- conf/conf.go
+++ conf/conf.go
@@ -8,18 +8,18 @@ import (
 )
 
 var (
-	GlobalConf *viper.Viper //使用单例模式实现配置实例
+	globalConf *viper.Viper //使用单例模式实现配置实例
 	confOnce   sync.Once
 )
 
-func ReadConfigFile(filename string) *viper.Viper {
+func readConfigFile(filename string) *viper.Viper {
 	confOnce.Do(func() {
-		GlobalConf = viper.New()
+		globalConf = viper.New()
 		//从项目的根目录下读取名为config，格式为yaml的文件
-		GlobalConf.SetConfigName(filename)
-		GlobalConf.SetConfigType("yaml")
-		GlobalConf.AddConfigPath(".")
-		err := GlobalConf.ReadInConfig()
+		globalConf.SetConfigName(filename)
+		globalConf.SetConfigType("yaml")
+		globalConf.AddConfigPath(".")
+		err := globalConf.ReadInConfig()
 		if err != nil {
 			var configFileNotFoundError viper.ConfigFileNotFoundError
 			if errors.As(err, &configFileNotFoundError) {
@@ -29,8 +29,8 @@ func ReadConfigFile(filename string) *viper.Viper {
 			}
 		}
 	})
-	return GlobalConf
+	return globalConf
 }
 func GetGlobalConf() *viper.Viper { //获取全局单例配置实例
-	return ReadConfigFile("config") //指定配置文件的文件名
+	return readConfigFile("config") //指定配置文件的文件名
 }
--- database/connect.go
+++ database/connect.go
@@ -11,19 +11,19 @@ import (
 var (
 	myConf   = conf.GetGlobalConf() //获取全局配置实例
 	dbOnce   sync.Once
-	GlobalDB *gorm.DB //全局数据库实例
+	globalDB *gorm.DB //全局数据库实例
 )
 
 func GetGlobalDB() *gorm.DB { //获取全局数据库单例实例
 	dbOnce.Do(func() {
 		dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", myConf.GetString("db.user"), myConf.GetString("db.passwd"), myConf.GetString("db.host"), myConf.GetString("db.port"), myConf.GetString("db.database"))
 		var err error
-		GlobalDB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		globalDB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 		if err != nil {
 			conf.GetGlobalLogger().Panicf("无法连接至数据库:%s:%s", myConf.GetString("db.host"), myConf.GetString("db.port"))
 		} else {
 			conf.GetGlobalLogger().Debugf("数据库连接成功")
 		}
 	})
-	return GlobalDB
+	return globalDB
 }
