ca66775fc74dd14e8109f1476540e6012e18ce48
DongJun <you@example.com>
2025-04-29 13:23:41 +0800

    通过配置文件动态设置web服务监听IP和端口

--- main.go
+++ main.go
@@ -5,8 +5,11 @@ import (
 	"awesomeProject/database"
 	"awesomeProject/model"
 	"awesomeProject/web"
+	"fmt"
 	"github.com/gin-gonic/gin"
 )
+
+var myConf = conf.GetGlobalConf()
 
 func main() {
 	//初始化数据库
@@ -16,9 +19,9 @@ func main() {
 		conf.GetGlobalLogger().Panicf("数据库迁移失败")
 	}
 	//初始化web
-	router := gin.Default()       //创建默认的Gin路由实例
-	router.GET("/ping", web.Ping) //web.Ping代表的是一个函数对象
-	err = router.Run()            //启动web服务且默认监听8080端口
+	router := gin.Default()                                                                            //创建默认的Gin路由实例
+	router.GET("/ping", web.Ping)                                                                      //web.Ping代表的是一个函数对象
+	err = router.Run(fmt.Sprintf("%s:%s", myConf.GetString("web.host"), myConf.GetString("web.port"))) //启动web服务且监听端口
 	if err != nil {
 		conf.GetGlobalLogger().Panicf("web服务启动失败:%s", err)
 	}
